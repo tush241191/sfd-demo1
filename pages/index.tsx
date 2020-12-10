059565472036688e4ca0196b5d8c9e426c5a5582
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-12-10 15:26:51 +0530

    [add] Added bottom buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,18 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 
+import { config as f_config, library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+f_config.autoAddCss = false;
+library.add(fas, fab, far);
+
 const Home = () => {
 	const url = "https://d5jmjyzrse4ui.cloudfront.net/assets/bg-mgm.webm";
-	
+
 	return (
 		<div className="relative bg-black overflow-hidden h-screen overflow-hidden z-10 border-b-0 border-white">
 			<video autoPlay muted loop id="myVideo" className="absolute top-0 left-0 min-w-full min-h-full w-auto h-auto ">
@@ -13,8 +22,10 @@ const Home = () => {
         </video>
 			<div className="relative h-full pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
 				<Navbar />
-				<div className="absolute bottom-5 w-full flex justify-center">
-					<button className="px-2 py-1 border border-white bg-transparent text-white text-sm">Button1</button>
+				<div className="absolute bottom-5 w-full flex justify-center space-x-2">
+					<img className="w-10 h-10" src="/arrow-circle-left-solid.svg"></img>
+					<img className="w-10 h-10" src="/arrow-circle-up-solid.svg"></img>
+					<img className="w-10 h-10" src="/arrow-circle-right-solid.svg"></img>
 				</div>
 			</div>
 		</div>
